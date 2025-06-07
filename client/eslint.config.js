544b8e4494d94b0637a388623cf96cb46c9b7f91
Joshua Daniel <jdaniel@gflenv.com>
2025-06-07 13:54:32 -0400

    Add prettier and eslint to client

---
+++ client/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+const config = defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    extends: ["js/recommended"],
+    plugins: { js },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
+
+export default config;
