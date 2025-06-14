589e8136538332160aaa8041acaca9ccf6be363c
Joshua Daniel <jdaniel@gflenv.com>
2025-06-14 10:16:28 -0400

    Swap out font

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -10,17 +10,11 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+// prettier-ignore
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+  { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
