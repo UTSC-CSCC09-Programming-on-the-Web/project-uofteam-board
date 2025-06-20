8fa5d2bae410f32894accd31afe14ca014420e98
Joshua Daniel <jdaniel@gflenv.com>
2025-06-20 12:13:39 -0400

    Small style changes and add login button to home page

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import { PiPencilDuotone } from "react-icons/pi";
+import { AiFillGoogleCircle } from "react-icons/ai";
 import { Button } from "~/components";
 
 export function meta() {
@@ -11,21 +12,17 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-yellow-50 flex items-center">
-      <div className="container mx-auto px-4 flex flex-col items-center text-center">
-        <h1 className="text-4xl font-extrabold">
-          Welcome to{" "}
-          <span className="text-blue-800 underline underline-offset-6">UofTeam Board</span>
-          <PiPencilDuotone className="inline text-blue-800 text-5xl" />
+      <div className="container mx-auto px-4 flex flex-col items-center text-center max-w-[768px]">
+        <h1 className="text-3xl md:text-4xl text-blue-800 font-extrabold">
+          <span className="underline underline-offset-6 decoration-blue-900">UofTeam Board</span>
+          <PiPencilDuotone className="inline text-4xl md:text-5xl" />
         </h1>
-        <p className="mt-4 text-xl text-gray-600">
+        <p className="mt-4 text-lg md:text-xl text-gray-600">
           A real-time collaborative canvas with AI-powered drawing completion—create, share, and
           enhance artwork together.
         </p>
-        <div className="flex gap-4 mt-8">
-          <Button onClick={() => navigate("/signup")}>Sign up</Button>
-          <Button onClick={() => navigate("/login")} variant="neutral">
-            Log in
-          </Button>
+        <div className="flex gap-4 mt-10">
+          <Button icon={<AiFillGoogleCircle size="1.3em" />}>Log in with Google</Button>
         </div>
       </div>
     </div>
