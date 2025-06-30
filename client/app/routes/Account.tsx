43fee0d77e00779741575b08f5cd7e510d9e41ee
Joshua Daniel <jdaniel@gflenv.com>
2025-06-30 14:49:31 -0400

    Add basic account and edit board page

--- client/app/routes/Account.tsx
+++ client/app/routes/Account.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+import type { Route } from "./+types/EditBoard";
+import type { User } from "~/types";
+import { API } from "~/services";
+
 export function meta() {
   return [{ title: "Account" }];
 }
 
-export async function clientLoader() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return;
-}
+export default function Account({ params }: Route.ComponentProps) {
+  const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
 
-export default function EditBoard() {
-  return <p>Load up account</p>;
+  useEffect(() => {
+    (async () => {
+      const res = await API.getMe();
+      if (res.error !== null) {
+        if (res.status === 401) navigate("/");
+        else alert(`Error fetching user: ${res.error}`);
+        return;
+      }
+
+      setUser(res.data);
+    })();
+  }, [params.bid]);
+
+  return user ? (
+    <>
+      <p>Loaded user</p>
+      <div className="mt-4 flex flex-col gap-2">
+        <p>ID: {user.id}</p>
+        <p>Name: {user.name}</p>
+        <p>Email: {user.email}</p>
+      </div>
+    </>
+  ) : (
+    <p>Loading user...</p>
+  );
 }
--- client/app/routes/EditBoard.tsx
+++ client/app/routes/EditBoard.tsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 import type { Route } from "./+types/EditBoard";
+import type { Board } from "~/types";
+import { API } from "~/services";
 
 export function meta() {
   return [{ title: "Edit Board" }];
 }
 
-export async function clientLoader() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return;
-}
-
 export default function EditBoard({ params }: Route.ComponentProps) {
-  return <p>Load up board {params.bid}</p>;
+  const [board, setBoard] = useState<Board | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const res = await API.getBoard(params.bid);
+      if (res.error !== null) {
+        if (res.status === 401) navigate("/");
+        else alert(`Error fetching board: ${res.error}`);
+        return;
+      }
+
+      setBoard(res.data);
+    })();
+  }, [params.bid]);
+
+  return board ? (
+    <>
+      <p>Loaded board {board.id}</p>
+      <div className="mt-4 flex flex-col gap-2">
+        <p>ID: {board.name}</p>
+        <p>Name: {board.name}</p>
+        <p>Created at: {board.createdAt}</p>
+        <p>Updated at: {board.updatedAt}</p>
+      </div>
+    </>
+  ) : (
+    <p>Loading board {params.bid}...</p>
+  );
 }
