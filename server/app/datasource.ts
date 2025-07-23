a0fda8787bf000806b40877f255fc0ab333385c6
Joshua Daniel <jdaniel@gflenv.com>
2025-07-23 18:57:21 -0400

    Try forcing ipv4

--- server/app/datasource.ts
+++ server/app/datasource.ts
@@ -9,6 +9,9 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "5432"),
     dialect: "postgres",
+    dialectOptions: {
+      family: 4,
+    },
     logging: (msg: string) => {
       console.log(msg);
     },
