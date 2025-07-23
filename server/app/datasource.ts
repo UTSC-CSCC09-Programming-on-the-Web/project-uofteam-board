1d973ea2ba34c858fe3539249970ed9d64aa1447
Joshua Daniel <jdaniel@gflenv.com>
2025-07-23 18:35:44 -0400

    Remove dialect and leave defaults

--- server/app/datasource.ts
+++ server/app/datasource.ts
@@ -9,12 +9,6 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "5432"),
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
     logging: (msg: string) => {
       console.log(msg);
     },
