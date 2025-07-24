881a5da387678e681828794d1495dce414f02452
Joshua Daniel <jdaniel@gflenv.com>
2025-07-24 10:41:01 -0400

    Add environment-based config

--- client/app/config.ts
+++ client/app/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  API_BASE_URL: "http://localhost:3000/api",
-  WS_BASE_URL: "http://localhost:3000",
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  WS_BASE_URL: import.meta.env.VITE_WS_BASE_URL,
 };
