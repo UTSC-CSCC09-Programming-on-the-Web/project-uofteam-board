390c42de96d2c87d4f435ad46c9df06ef0f98f91
Joshua Daniel <jdaniel@gflenv.com>
2025-07-25 18:11:58 -0400

    Add underline animation

--- client/app/routes/Home/Home.tsx
+++ client/app/routes/Home/Home.tsx
@@ -1,8 +1,12 @@
+import clsx from "clsx";
 import { useSearchParams } from "react-router";
 import { PiPencilDuotone } from "react-icons/pi";
 import { AiFillGoogleCircle } from "react-icons/ai";
+
 import { Button } from "~/components";
 import { API } from "~/services";
+
+import styles from "./Home.module.css";
 
 export function meta() {
   return [{ title: "UofTeam Board" }];
@@ -16,9 +20,9 @@ export default function Home() {
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#eab30850_1px,transparent_1px),linear-gradient(to_bottom,#eab30850_1px,transparent_1px)] bg-[size:48px_48px]" />
       <div className="absolute inset-0 bg-radial from-yellow-50 to-transparent" />
       <div className="z-10 px-4 pb-16 flex flex-col items-center text-center max-w-3xl">
-        <h1 className="text-3xl md:text-4xl text-blue-800 font-extrabold">
-          <span className="underline underline-offset-6 decoration-blue-900">UofTeam Board</span>
-          <PiPencilDuotone className="inline text-4xl md:text-5xl" />
+        <h1 className="text-3xl md:text-4xl text-blue-800 font-extrabold relative">
+          <span className={styles.underline}>UofTeam Board</span>
+          <PiPencilDuotone className={clsx("inline text-4xl md:text-5xl", styles.logo)} />
         </h1>
         <p className="mt-4 text-lg md:text-xl text-gray-600">
           A real-time collaborative canvas with AI-powered drawing completion—create, share, and
