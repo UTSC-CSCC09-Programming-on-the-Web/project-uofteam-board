486a90fe56e18fe7d49c8cdc15cf90a1f7ebd1a2
Joshua Daniel <jdaniel@gflenv.com>
2025-07-26 18:45:16 -0400

    Clean up dashboard and improve error showing

--- client/app/routes/Dashboard/Dashboard.tsx
+++ client/app/routes/Dashboard/Dashboard.tsx
@@ -1,76 +1,79 @@
 import { useEffect, useRef, useState } from "react";
-import { MdDraw } from "react-icons/md";
-import { FiPlus } from "react-icons/fi";
 import { useNavigate } from "react-router";
+import { FiPlus } from "react-icons/fi";
+import toast from "react-hot-toast";
 
 import { API } from "~/services";
 import { Button } from "~/components";
 import { type Paginated, type Board } from "~/types";
 
+import type { Route } from "./+types/Dashboard";
 import { QueryInput } from "./QueryInput";
 import { BoardCard } from "./BoardCard";
+import { EmptyState } from "./EmptyState";
+import { sleep } from "~/utils";
 
-export function meta() {
-  return [{ title: "Dashboard" }];
-}
+const meta: Route.MetaFunction = () => [{ title: "Dashboard" }];
 
-export default function Dashboard() {
+const Dashboard = () => {
   const navigate = useNavigate();
   const [query, setQuery] = useState("");
   const [boards, setBoards] = useState<Board[]>([]);
   const [boardsLoading, setBoardsLoading] = useState(false);
   const [creatingNewBoard, setCreatingNewBoard] = useState(false);
-  const [lastPaginatedResponse, setLastPaginatedResponse] = useState<Paginated<Board> | null>(null);
-
+  const [pagination, setPagination] = useState<Paginated<Board> | null>(null);
   const latestReqID = useRef(0);
+
   useEffect(() => {
     (async () => {
       setBoardsLoading(true);
       const reqID = ++latestReqID.current;
-      const res = await API.getBoards(0, 6, query);
+      // Sleep ensures the loading state is not visually jarring
+      const [, res] = await Promise.all([sleep(300), API.getBoards(0, 6, query)]);
       if (reqID !== latestReqID.current) return;
 
       if (res.error !== null) {
         if (res.status === 401) {
-          navigate("/");
+          navigate("/?error=not_logged_in");
+        } else if (res.status === 403) {
+          navigate("/account");
         } else {
-          alert(`Error fetching boards: ${res.error}`);
+          toast(`Failed to fetch boards:\n ${res.error}`);
           setBoardsLoading(false);
         }
         return;
       }
 
-      setLastPaginatedResponse(res.data);
+      setPagination(res.data);
       setBoards(res.data.data);
       setBoardsLoading(false);
     })();
   }, [navigate, query]);
 
-  const onLoadMore = async () => {
-    if (!lastPaginatedResponse?.nextPage) return;
+  const handleLoadMore = async () => {
+    if (!pagination?.nextPage) return;
 
     setBoardsLoading(true);
     const reqID = ++latestReqID.current;
-    const res = await API.getBoards(lastPaginatedResponse.nextPage, lastPaginatedResponse.limit, query); // prettier-ignore
+    const res = await API.getBoards(pagination.nextPage, pagination.limit, query);
     if (reqID !== latestReqID.current) return;
     if (res.error !== null) {
-      alert(`Error loading more boards: ${res.error}`);
+      toast(`Failed to fetch boards:\n ${res.error}`);
       setBoardsLoading(false);
       return;
     }
 
-    setLastPaginatedResponse(res.data);
+    setPagination(res.data);
     setBoards((prev) => [...prev, ...res.data.data]);
     setBoardsLoading(false);
   };
 
-  // TODO: add option to choose board name
-  const onNewBoard = async () => {
+  const handleCreateBoard = async () => {
     setCreatingNewBoard(true);
-    const res = await API.createBoard((Math.random() + 1).toString(36).substring(2));
+    const res = await API.createBoard("Untitled board");
     setCreatingNewBoard(false);
     if (res.error !== null) {
-      alert(`Error creating new board: ${res.error}`);
+      toast(`Failed to create board:\n ${res.error}`);
       return;
     }
 
@@ -81,37 +84,19 @@ export default function Dashboard() {
     <>
       <div className="flex items-center justify-between">
         <h1 className="text-xl md:text-2xl font-extrabold text-blue-800">Dashboard</h1>
-        <Button icon={<FiPlus />} size="sm" onClick={onNewBoard} loading={creatingNewBoard}>
+        <Button icon={<FiPlus />} size="sm" onClick={handleCreateBoard} loading={creatingNewBoard}>
           New board
         </Button>
       </div>
       <QueryInput loading={boardsLoading} onChange={setQuery} className="mt-4" />
       {boards.length === 0 ? (
-        <div className="mt-16 flex flex-col items-center text-center">
-          <MdDraw className="size-24 rotate-30 text-yellow-800/25" />
-          <p className="mt-4 mb-2 text-xl font-medium text-yellow-950/70">
-            {boardsLoading ? "Loading boards..." : "No boards found."}
-          </p>
-          {!boardsLoading && (
-            <>
-              <p className="text-yellow-950/60">
-                {query
-                  ? "Try changing your search query or creating a new board."
-                  : "Create a new board to get started!"}
-              </p>
-              <Button
-                icon={<FiPlus />}
-                size="sm"
-                variant="neutral"
-                className="mt-6"
-                onClick={onNewBoard}
-                loading={creatingNewBoard}
-              >
-                New board
-              </Button>
-            </>
-          )}
-        </div>
+        <EmptyState
+          query={query}
+          loadingBoards={boardsLoading}
+          creatingNewBoard={creatingNewBoard}
+          onCreateBoard={handleCreateBoard}
+          className="mt-16"
+        />
       ) : (
         <div className="flex flex-col items-center justify-between mt-4 sm:mt-8">
           <div className="w-full grid gap-4 sm:gap-6 lg:grid-cols-2 xl:grid-cols-3">
@@ -139,12 +124,12 @@ export default function Dashboard() {
               />
             ))}
           </div>
-          {lastPaginatedResponse?.nextPage && (
+          {pagination?.nextPage && (
             <Button
               className="mt-16"
               variant="secondary"
-              onClick={onLoadMore}
               loading={boardsLoading}
+              onClick={handleLoadMore}
             >
               Load more
             </Button>
@@ -153,4 +138,6 @@ export default function Dashboard() {
       )}
     </>
   );
-}
+};
+
+export { Dashboard as default, meta };
